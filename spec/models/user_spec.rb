1de3555f07d677eb50ea103877a9d7f9a88c5cb8
paulobarbosa93 <paulo.n.barbosa2@gmail.com>
2018-12-03 00:44:50 +0000

    adding shoulda matchers to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { build(:user) }
+  let(:user) { build(:user) }
 
-  it { expect(subject).to respond_to(:email) }
-  it { is_expected.to respond_to(:email) } # testa uma instancia sem usar o factory
-  it { expect(subject).to respond_to(:name) }
-  it { expect(subject).to respond_to(:password) }
-  it { expect(subject).to respond_to(:password_confirmation) }
-  it { expect(subject).to be_valid }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value('paulo.n.barbosa2@gmail.com').for(:email) }
 end
