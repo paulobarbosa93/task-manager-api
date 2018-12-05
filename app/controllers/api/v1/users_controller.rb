7788cd5711a951ccc5b87e9ec7eab587a04d607a
paulobarbosa93 <paulo.n.barbosa2@gmail.com>
2018-12-05 04:14:43 +0000

    adding create action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,29 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show]
   respond_to :json
 
   def show
-    @user = User.find(params[:id])
     respond_with @user
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   rescue StandardError
     head 404
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
