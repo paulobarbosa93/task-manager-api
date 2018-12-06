2ab2492c7cb8815d880f5a658cc5993f154de558
paulobarbosa93 <paulo.n.barbosa2@gmail.com>
2018-12-06 20:21:21 +0000

    adding user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,10 +10,28 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:auth_token) }
 
   describe '#info' do
-    it 'returns email and created_at' do
+    it 'returns email, created_at and token' do
       user.save!
       allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
       expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: #{Devise.friendly_token}")
+    end
+  end
+
+  describe '#generate_authentication_token!' do
+    it 'generates a unique auth' do
+      user.save!
+      allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+      user.generate_authentication_token!
+
+      expect(user.auth_token).to eq('abc123xyzTOKEN')
+    end
+
+    it 'generates another auth token when the current auth token already has been taken' do
+      allow(Devise).to receive(:friendly_token).and_return('abc123TOKENxyz', 'abc123TOKENxyz', 'abcXYZ123456789')
+      existing_user = create(:user)
+      user.generate_authentication_token!
+
+      expect(user.auth_token).not_to eq(existing_user.auth_token)
     end
   end
 end
