63157dedf3bf59b5020c2ff9db98e301d06723e9
paulobarbosa93 <paulo.n.barbosa2@gmail.com>
2018-12-18 02:58:27 +0000

    adding Api::V2

---
+++ app/controllers/api/v2/tasks_controller.rb
@@ -0,0 +1,43 @@
+class Api::V2::TasksController < ApplicationController
+  before_action :authenticate_with_token!
+
+  def index
+    tasks = current_user.tasks
+    render json: { tasks: tasks }
+  end
+
+  def show
+    task = current_user.tasks.find(params[:id])
+    render json: task
+  end
+
+  def create
+    task = current_user.tasks.build(task_params)
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    task = current_user.tasks.find(params[:id])
+    if task.update(task_params)
+      render json: task
+    else
+      render json: { errors: task.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    task = current_user.tasks.find(params[:id])
+    task.destroy
+    head :no_content
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :deadline, :done)
+  end
+end
---
+++ app/controllers/api/v2/users_controller.rb
@@ -0,0 +1,44 @@
+class Api::V2::UsersController < ApplicationController
+  before_action :authenticate_with_token!, only: [:update, :destroy]
+  before_action :set_user, only: [:show]
+  respond_to :json
+
+  def show
+    respond_with @user
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user = current_user
+    if user.update(user_params)
+      render json: user
+    else
+      render json: { errors: user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    current_user.destroy
+    head 204
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue StandardError
+    head 404
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
