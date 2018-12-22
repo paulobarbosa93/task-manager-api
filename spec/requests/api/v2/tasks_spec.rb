7d99e6ec698d3f31d7e54313b50e76a45014b4b3
paulobarbosa93 <paulo.n.barbosa2@gmail.com>
2018-12-22 21:27:17 +0000

    adding ransack for tasks

--- spec/requests/api/v2/tasks_spec.rb
+++ spec/requests/api/v2/tasks_spec.rb
@@ -13,22 +13,40 @@ RSpec.describe 'Tasks API', type: :request do
     }
   end
 
-  describe "GET /tasks" do
-    before do
-      create_list(:task, 5, user_id: user.id)
-      get '/tasks', params: {}, headers: headers
+  describe 'GET /tasks' do
+    context 'when no filter params is sent' do
+      before do
+        create_list(:task, 5, user_id: user.id)
+        get '/tasks', params: {}, headers: headers
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns 5 tasks from database' do
+        expect(json_body[:data].count).to eq(5)
+      end
     end
 
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
-    end
+    context 'when filter params is sent' do
+      let!(:notbook_task_1) { create(:task, title: 'Check if the notebook is broken', user_id: user.id) }
+      let!(:notbook_task_2) { create(:task, title: 'Buy a new notebook', user_id: user.id) }
+      let!(:other_task_1) { create(:task, title: 'Fix the door', user_id: user.id) }
+      let!(:other_task_2) { create(:task, title: 'Buy a new car', user_id: user.id) }
 
-    it 'returns 5 tasks from database' do
-      expect(json_body[:data].count).to eq(5)
+      before do
+        get '/tasks?q[title_cont]=note', params: {}, headers: headers
+      end
+
+      it 'returns only the tasks matching' do
+        returned_task_titles = json_body[:data].map { |t| t[:attributes][:title] }
+        expect(returned_task_titles).to eq([notbook_task_1.title, notbook_task_2.title])
+      end
     end
   end
 
-  describe "GET /tasks/:id" do
+  describe 'GET /tasks/:id' do
     let(:task) { create(:task, user_id: user.id) }
 
     before do
@@ -44,14 +62,14 @@ RSpec.describe 'Tasks API', type: :request do
     end
   end
 
-  describe "POST /tasks" do
+  describe 'POST /tasks' do
     let(:task_params) { attributes_for(:task) }
 
     before do
       post '/tasks', params: { task: task_params }.to_json, headers: headers
     end
 
-    context "when the params are valid" do
+    context 'when the params are valid' do
       it 'returns status code 201' do
         expect(response).to have_http_status(201)
       end
@@ -69,7 +87,7 @@ RSpec.describe 'Tasks API', type: :request do
       end
     end
 
-    context "when the params are invalid" do
+    context 'when the params are invalid' do
       let(:task_params) { attributes_for(:task, title: '') }
 
       it 'returns status code 422' do
@@ -86,58 +104,58 @@ RSpec.describe 'Tasks API', type: :request do
     end
   end
 
-  describe "PUT /tasks/:id" do
+  describe 'PUT /tasks/:id' do
     let!(:task) { create(:task, user_id: user.id) }
 
     before do
       put "/tasks/#{task.id}", params: { task: task_params }.to_json, headers: headers
     end
 
-    context "when the params are valid" do
+    context 'when the params are valid' do
       let(:task_params) { { title: 'New task title' } }
 
-      it "returns status code 200" do
+      it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
 
-      it "return the json for updated task" do
+      it 'return the json for updated task' do
         expect(json_body[:data][:attributes][:title]).to eq task_params[:title]
       end
 
-      it "updates the task in the database" do
+      it 'updates the task in the database' do
         expect(Task.find_by(title: task_params[:title])).not_to be_nil
       end
     end
 
-    context "when the params are invalid" do
+    context 'when the params are invalid' do
       let(:task_params) { { title: '' } }
 
-      it "returns status code 422" do
+      it 'returns status code 422' do
         expect(response).to have_http_status(422)
       end
 
-      it "return the json error for title" do
+      it 'return the json error for title' do
         expect(json_body[:errors]).to have_key(:title)
       end
 
-      it "does not update the task in database" do
+      it 'does not update the task in database' do
         expect(Task.find_by(title: task_params[:title])).to be_nil
       end
     end
   end
 
-  describe "DELETE /tasks/:id" do
+  describe 'DELETE /tasks/:id' do
     let!(:task) { create(:task, user_id: user.id) }
 
     before do
       delete "/tasks/#{task.id}", params: {}, headers: headers
     end
 
-    it "returns status code 204" do
+    it 'returns status code 204' do
       expect(response).to have_http_status(204)
     end
 
-    it "removes the task form the database" do
+    it 'removes the task form the database' do
       expect { Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
