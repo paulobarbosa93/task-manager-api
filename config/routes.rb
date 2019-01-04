adcd60f8be838e35c78b156ff33fca55035e39fd
paulobarbosa93 <paulo.n.barbosa2@gmail.com>
2019-01-04 02:12:21 +0000

    removing api route constraint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_version_constraint'
 Rails.application.routes.draw do
   devise_for :users, only: [:sessions], controllers: { sessions: 'api/v1/sessions' }
 
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json }, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1) do
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
     namespace :v2, path: '/', constraints: ApiVersionConstraint.new(version: 2, default: true) do
       mount_devise_token_auth_for 'User', at: 'auth'
-      
+
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
       resources :tasks, only: [:index, :show, :create, :update, :destroy]
